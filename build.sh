fada43ec54caee36d5342461fe7d52241eeec772
lonelyxgk <w468767577@outlook.com>
2026-07-02 17:30:31 +0800

    feat: build script auto zip archive

--- build.sh
+++ build.sh
@@ -18,4 +18,42 @@ export AR_x86_64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linu
 export LD_x86_64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/ld
 export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=${CC_x86_64_linux_android}
 
-cargo build --target aarch64-linux-android --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu --target riscv64gc-unknown-linux-gnu --target x86_64-unknown-linux-musl --target x86_64-pc-windows-gnu --release
+cargo build --target aarch64-linux-android --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu --target riscv64gc-unknown-linux-gnu --target x86_64-unknown-linux-musl --target x86_64-pc-windows-gnu --target x86_64-linux-android --release
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+VERSION="v1.0.1"
+
+[ ! -d "./target/build" ] && mkdir ./target/build
+
+targets=(
+    "aarch64-linux-android"
+    "x86_64-linux-android"
+    "aarch64-unknown-linux-gnu"
+    "riscv64gc-unknown-linux-gnu"
+    "x86_64-unknown-linux-musl"
+    "x86_64-pc-windows-gnu"
+)
+current=$(pwd)
+for target in "${targets[@]}"
+do  
+    if [[ $target =~ "windows" ]]; then
+        (
+            cd "./target/${target}/release" || exit 1
+            zip "ddns-${VERSION}-${target}.zip" ddns.exe
+            cp "ddns-${VERSION}-${target}.zip" ${current}/target/build
+        )
+    else
+        (
+            cd "./target/${target}/release" || exit 1
+            zip "ddns-${VERSION}-${target}.zip" ddns
+            cp "ddns-${VERSION}-${target}.zip" ${current}/target/build
+        )
+    fi
+
+    echo "$target"
+done
+
+
