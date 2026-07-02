c11f95ddbf2fe5a6c0b2447d217886baee9f8f93
lonelyxgk <w468767577@outlook.com>
2026-07-02 16:55:17 +0800

    fix: CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER

--- build.sh
+++ build.sh
@@ -9,11 +9,13 @@ fi
 export CC_aarch64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang
 export CXX_aarch64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang++
 export AR_aarch64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINUX_ANDROID_LINKER=${CC_aarch64_linux_android}
+export LD_aarch64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/ld
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=${CC_aarch64_linux_android}
 
 export CC_x86_64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android35-clang
 export CXX_x86_64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android35-clang++
 export AR_x86_64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
+export LD_x86_64_linux_android=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/ld
 export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=${CC_x86_64_linux_android}
 
-cargo build --target aarch64-linux-android --target x86_64-unknown-linux-gnu --target  aarch64-unknown-linux-gnu --target riscv64gc-unknown-linux-gnu --target x86_64-unknown-linux-musl --target x86_64-pc-windows-gnu --release
+cargo build --target aarch64-linux-android --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu --target riscv64gc-unknown-linux-gnu --target x86_64-unknown-linux-musl --target x86_64-pc-windows-gnu --release
